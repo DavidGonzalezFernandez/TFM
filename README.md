eb694022529dcfa6b81f0bab2c83fd3113e0c8f7
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2024-06-27 20:56:33 +0300

    Final version of files

--- README.md
+++ README.md
@@ -1,20 +1,17 @@
-# TFM
+# Evaluación de la Librería TINTOlib
 
 ## Descripción general
-Este repositorio contiene el código, los recursos y los resultados de los experimentos realizados en mi Trabajo de Fin de Máster.
+Este repositorio contiene el código y los recursos para replicar los experimentos realizados en mi Trabajo de Fin de Máster.
 
 ## Ficheros y carpetas
 Ficheros:
+- `utils.py`: contiene funciones para cargar los datasets; hacer los splits de entrenamiento, valicación y test; obtener información sobre los datasets; crear las ramas de los modelos
 - `dataset_information.ipynb`: notebook para probar a cargar los dataset y obtener información sobre la cantidad y tipo de datos.
-- `datasets_to_images.py`: script para transformar los datasets en imágenes utilizando los métodos disponibles en TINTOlib. 
-- `train_classic.ipynb`: notebook para entrenar descriptores clásicos con el conjunto de entrenamiento para los datasets disponibles.
-- `train_classic_with_split1.ipynb`: notebook para entrenar descriptores clásicos con parte del conjunto de entrenamiento para los datasets disponibles.
-- `train_cnn.ipynb`: notebook para entrenar CNNs a partir de las imágenes.
-- `train_cnn_fnn.ipynb`: notebook para entrenar modelos CNN+FFNN a partir de las imágenes y los datos del dataset. El entrenamiento se realiza con las imágenes generadas con `datasets_to_images.py` (en la rama CNN) y los datos del dataset (en la rama FFNN).
-- `train_cnn_classic.ipynb`: notebook para entrenar modelos CNN+descriptores a partir de las imágenes y los datos del dataset. El entrenamiento se realiza con las imágenes generadas con `datasets_to_images.py` (en la rama CNN) y las predicciones que los modelos de `train_classic_with_split1.ipynb` devuelven (concatenados a la predicción de la rama CNN).
-- `utils.py`: contiene funciones para cargar los datasets; hacer los splits de entrenamiento, valicación y test; y obtener información sobre el tipo de dataset.
+- `evaluate_models.ipynb`: notebook para evaluar los modelos entrenados
+- `train_classic.ipynb`: notebook para entrenar descriptores clásicos con los datos de un dataset.
+- `train_cnn.ipynb`: notebook para entrenar CNNs a partir de imágenes sintéticas.
+- `train_cnn_mlp.ipynb`: notebook para entrenar modelos CNN+MLP a partir de las imágenes sintéticas y los datos de un dataset. El entrenamiento se realiza con las imágenes sintéticas (en la rama CNN) y los datos del dataset (en la rama FFNN).
+- `train_cnn_ML.ipynb`: notebook para entrenar modelos CNN+ML a partir de las imágenes sintéticas y los datos de un dataset. El entrenamiento se realiza con las imágenes sintéticas (en la rama CNN) y las predicciones que de un modelo RandomForest devuelve (concatenados a la predicción de la rama CNN).
 
 Carpetas:
 - `datasets`: contiene los datasets en bruto
-- `images`: contiene los datasets de `utils` convertidos a imágenes.
-- `results`: contiene los resultados obtenidos en los scripts y notebooks.
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import sklearn.datasets
 import os
 from typing import Dict, Optional
 import numpy as np
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, BatchNormalization, Input, Activation,MaxPooling2D, AveragePooling2D, Concatenate
 
 pd.set_option('future.no_silent_downcasting', True)
 
@@ -12,14 +13,17 @@ CALIFORNIA_HOUSING_NAME = "California_Housing"
 DENGUE_DATASET = "Dengue_Chikungunya"
 COVERTYPE_NAME = "Covertype"
 
+# Strings representing each method
 IGTD_NAME = "IGTD"
 REFINED_NAME = "REFINED"
 BARGRAPH_NAME = "BARGRAPH"
-DISTANCE_MATRIX_NAME = "DISTANCE_MATRIX"
+DISTANCE_MATRIX_NAME = "DISTANCE-MATRIX"
 COMBINATION_NAME = "COMBINATION"
 TINTO_NAME = "TINTO"
-SUPERTML_EF_NAME = "SuperTML_EF"
-SUPERTML_VF_NAME = "SuperTML_VF"
+SUPERTML_EF_NAME = "SuperTML-EF"
+SUPERTML_VF_NAME = "SuperTML-VF"
+
+DROPOUT_VALUE = 0.2
 
 """List with all the available datasets"""
 ALL_DATASETS = [HELOC_NAME, CALIFORNIA_HOUSING_NAME, DENGUE_DATASET, COVERTYPE_NAME]
@@ -30,10 +34,10 @@ ALL_IMAGE_METHODS = [IGTD_NAME, REFINED_NAME, BARGRAPH_NAME, DISTANCE_MATRIX_NAM
 """Seed for reproducibility in shuffling and spliting"""
 RANDOM_SEED = 1234
 
-"""Size of test split (in percentage)"""
+"""Size of test split (out of 1)"""
 TEST_SPLIT_SIZE = 0.1
 
-"""Size of validation split (in percentage)"""
+"""Size of validation split (out of 1)"""
 VAL_SPLIT_SIZE = 0.1
 
 """List with the datasets that are binary classification"""
@@ -42,10 +46,10 @@ assert all(c in ALL_DATASETS for c in BINARY_CLASSIFICATION_DATASETS)
 
 """Dictionary with pairs of dataset key and number of samples"""
 N_SAMPLES_PER_DATASET: Dict[str, int] = {
-    HELOC_NAME: 9871,
-    CALIFORNIA_HOUSING_NAME: 20640,
-    DENGUE_DATASET: 11448,
-    COVERTYPE_NAME: 581012,
+    HELOC_NAME: 9_871,
+    CALIFORNIA_HOUSING_NAME: 20_640,
+    DENGUE_DATASET: 11_448,
+    COVERTYPE_NAME: 581_012,
 }
 assert set(ALL_DATASETS) == set(N_SAMPLES_PER_DATASET.keys())
 
@@ -64,40 +68,41 @@ DATASETS_N_CLASSES: Dict[str, Optional[int]] = {
     DENGUE_DATASET: 2,
     COVERTYPE_NAME: 7,
 }
+assert set(ALL_DATASETS) == set(DATASET_TYPES.keys())
 
 def get_X_y(dataset_name: str):
     """Given the string that represents a dataset, returns X and y"""
     if dataset_name not in ALL_DATASETS:
         raise ValueError("Cannot find a dataset with that name.")
-    
+
     elif dataset_name == HELOC_NAME:
         path_to_dataset = os.path.join("datasets", "heloc.csv")
         df = pd.read_csv(path_to_dataset)
         label_col = "RiskPerformance"
         X = df.drop(label_col, axis=1).to_numpy()
         y = df[label_col].replace({"Good": 0, "Bad": 1}).astype(int).to_numpy()
-    
+
     elif dataset_name == COVERTYPE_NAME:
         X,y = sklearn.datasets.fetch_covtype(return_X_y=True)
         y = y-1
-    
+
     elif dataset_name == CALIFORNIA_HOUSING_NAME:
         X, y = sklearn.datasets.fetch_california_housing(return_X_y=True)
-    
+
     elif dataset_name == DENGUE_DATASET:
         path_to_dataset = os.path.join("datasets", "arboviruses_dataset.csv")
         df = pd.read_csv(path_to_dataset, sep=";")
-        
+
         label_col = "CLASSI_FIN"
         desired_targets = ['CHIKUNGUNYA', 'DENGUE']
         df = df[df[label_col].isin(desired_targets)]
 
         X = df.drop(label_col, axis=1).to_numpy()
         y = sklearn.preprocessing.LabelEncoder().fit_transform(df[label_col])
-    
+
     else:
         raise NotImplementedError()
-    
+
     assert X.shape[0] == N_SAMPLES_PER_DATASET[dataset_name]
     return X,y
 
@@ -105,7 +110,7 @@ def get_indices_train_eval(dataset_name: str):
     total_elems: int = N_SAMPLES_PER_DATASET.get(dataset_name, -1)
     if total_elems == -1:
         raise ValueError("Cannot find a dataset with that name.")
-    
+
     # Create the array with all the indices
     indices = np.arange(total_elems)
 
@@ -128,34 +133,14 @@ def get_indices_train1_eval1_train2_eval2(dataset_name: str):
     if total_elems == -1:
         raise ValueError("Cannot find a dataset with that name.")
 
-    # Create the array with all the indices
-    indices = np.arange(total_elems)
-
-    # Remove the indices from eval
-    indices_test = get_indices_test(dataset_name)
-    non_test_indices = np.setdiff1d(indices, indices_test)
-    assert indices_test.shape[0] + non_test_indices.shape[0] == total_elems
-
-    # Split 50% for 1 and 50% for 2
-    split_size = non_test_indices.shape[0] // 2
-    split_1, split_2 = non_test_indices[:split_size], non_test_indices[split_size:]
-    assert split_1.shape[0] + split_2.shape[0] == non_test_indices.shape[0]
-
-    r = np.random.default_rng(seed=RANDOM_SEED)
-    n_val = int(total_elems * VAL_SPLIT_SIZE)//2
-
-    # Get train 1 and eval 1
-    r.shuffle(split_1)
-    train_1, val_1 = split_1[:-n_val], split_1[-n_val:]
-    assert train_1.shape[0] + val_1.shape[0] == split_1.shape[0]
-
-    # Get train2 and eval 2
-    r.shuffle(split_2)
-    train_2, val_2 = split_2[:-n_val], split_2[-n_val:]
-    assert train_2.shape[0] + val_2.shape[0] == split_2.shape[0]
+    # Get train_val indices
+    train_indices, val_indices = get_indices_train_eval(dataset_name)
+    # Split train_val indices in half
+    train_1, train_2 = train_indices[:train_indices.shape[0]//2], train_indices[train_indices.shape[0]//2:]
+    val_1, val_2 = val_indices[:val_indices.shape[0]//2], val_indices[val_indices.shape[0]//2:]
 
     # Return
-    assert train_1.shape[0] + val_1.shape[0] + train_2.shape[0] + val_2.shape[0] == non_test_indices.shape[0]
+    assert train_1.shape[0] + val_1.shape[0] + train_2.shape[0] + val_2.shape[0] == train_indices.shape[0] + val_indices.shape[0]
     return train_1, val_1, train_2, val_2
 
 def get_indices_test(dataset_name: str):
@@ -176,14 +161,14 @@ def get_indices_test(dataset_name: str):
 
 def get_results_path():
     p = "results"
-    if p not in os.listdir():
-        os.mkdir(p)
+    if not os.path.exists(p):
+        os.makedirs(p)
     return p
 
 def get_images_base_path():
     p = "images"
     if not os.path.exists(p):
-        os.mkdir(p)
+        os.makedirs(p)
     return p
 
 def get_images_path_for_dataset(dataset_name, image_method):
@@ -191,66 +176,80 @@ def get_images_path_for_dataset(dataset_name, image_method):
         raise Exception("This dataset does not exist")
     if image_method not in ALL_IMAGE_METHODS:
         raise Exception("This image method does not exist")
-    p = os.path.join(get_images_base_path(), dataset_name)
+    p = os.path.join(get_images_base_path(), dataset_name, image_method)
     if not os.path.exists(p):
-        os.mkdir(p)
-    p = os.path.join(p, image_method)
-    if not os.path.exists(p):
-        os.mkdir(p)
-    return p
-
-def get_classicdescriptors_base_path():
-    p = os.path.join(get_results_path(), "classic_descriptors")
-    if not os.path.exists(p):
-        os.mkdir(p)
+        os.makedirs(p)
     return p
 
 def get_classicdescriptors_path(dataset_name):
+    def get_classicdescriptors_base_path():
+        p = os.path.join(get_results_path(), "classic_descriptors")
+        if not os.path.exists(p):
+            os.makedirs(p)
+        return p
+    
     if dataset_name not in ALL_DATASETS:
         raise Exception("This dataset does not exist")
     p = os.path.join(get_classicdescriptors_base_path(), dataset_name)
     if not os.path.exists(p):
-        os.mkdir(p)
+        os.makedirs(p)
     return p
 
-def get_classicdescriptors_split1_base_path():
-    p = os.path.join(get_results_path(), "classic_descriptors_split1")
-    if not os.path.exists(p):
-        os.mkdir(p)
-    return p
 
 def get_classicdescriptors_split1_path(dataset_name):
+    def get_classicdescriptors_split1_base_path():
+        p = os.path.join(get_results_path(), "classic_descriptors_split1")
+        if not os.path.exists(p):
+            os.makedirs(p)
+        return p
+    
     if dataset_name not in ALL_DATASETS:
         raise Exception("This dataset does not exist")
     p = os.path.join(get_classicdescriptors_split1_base_path(), dataset_name)
     if not os.path.exists(p):
-        os.mkdir(p)
-    return p
-
-def get_cnnmodels_base_path():
-    p = os.path.join(get_results_path(), "cnn_models")
-    if not os.path.exists(p):
-        os.mkdir(p)
+        os.makedirs(p)
     return p
 
 def get_cnnmodels_path(dataset_name):
+    def get_cnnmodels_base_path():
+        p = os.path.join(get_results_path(), "cnn_models")
+        if not os.path.exists(p):
+            os.makedirs(p)
+        return p
+    
     if dataset_name not in ALL_DATASETS:
         raise Exception("This dataset does not exist")
     p = os.path.join(get_cnnmodels_base_path(), dataset_name)
     if not os.path.exists(p):
-        os.mkdir(p)
+        os.makedirs(p)
     return p
 
-def get_cnnfnnmodels_path():
-    p = os.path.join(get_results_path(), "cnn_ffnn_models")
+def get_cnnmlp_models_path(dataset_name):
+    def get_cnnmlp_models_base_path():
+        p = os.path.join(get_results_path(), "cnn_mlp_models")
+        if not os.path.exists(p):
+            os.makedirs(p)
+        return p
+    
+    if dataset_name not in ALL_DATASETS:
+        raise Exception("This dataset does not exist")
+    p = os.path.join(get_cnnmlp_models_base_path(), dataset_name)
     if not os.path.exists(p):
-        os.mkdir(p)
+        os.makedirs(p)
     return p
 
-def get_cnn_classicdescriptors_path():
-    p = os.path.join(get_results_path(), "cnn_classic_models")
+def get_cnn_classicdescriptors_path(dataset_name):
+    def get_cnn_classicdescriptors_base_path():
+        p = os.path.join(get_results_path(), "cnn_classic_models")
+        if not os.path.exists(p):
+            os.makedirs(p)
+        return p
+    
+    if dataset_name not in ALL_DATASETS:
+        raise Exception("This dataset does not exist")
+    p = os.path.join(get_cnn_classicdescriptors_base_path(), dataset_name)
     if not os.path.exists(p):
-        os.mkdir(p)
+        os.makedirs(p)
     return p
 
 def get_dataset_type_str(dataset_name: str):
@@ -283,6 +282,93 @@ def get_number_of_classes(dataset_name: str):
         assert n>2
         return n
     raise Exception("This shouldn't have happened.")
+
+def get_cnn_branch(input_shape):
+    """Returns the CNN branches"""
+    # Input layer
+    input_branch = Input(input_shape)
+
+    # Start branch 1
+    tower_1 = Conv2D(16, (3,3), activation='relu',padding="same")(input_branch)
+    tower_1 = BatchNormalization()(tower_1)
+    tower_1 = Activation('relu')(tower_1)
+    tower_1 = MaxPooling2D(2,2)(tower_1)
+    tower_1 = Dropout(DROPOUT_VALUE)(tower_1)
+
+    tower_1 = Conv2D(32, (3,3), activation='relu',padding="same")(tower_1)
+    tower_1 = BatchNormalization()(tower_1)
+    tower_1 = Activation('relu')(tower_1)
+    tower_1 = MaxPooling2D(2,2)(tower_1)
+    tower_1 = Dropout(DROPOUT_VALUE)(tower_1)
+
+    tower_1 = Conv2D(64, (3,3), activation='relu',padding="same")(tower_1)
+    tower_1 = BatchNormalization()(tower_1)
+    tower_1 = Activation('relu')(tower_1)
+    tower_1 = MaxPooling2D(2,2)(tower_1)
+    tower_1 = Dropout(DROPOUT_VALUE)(tower_1)
+
+    tower_1 = Conv2D(64, (3,3), activation='relu',padding="same")(tower_1)
+    tower_1 = BatchNormalization()(tower_1)
+    tower_1 = Activation('relu')(tower_1)
+    tower_1 = MaxPooling2D(2,2)(tower_1)
+    tower_1 = Dropout(DROPOUT_VALUE)(tower_1)
+    # End branch 1
+
+    # Start branch 2
+    tower_2 = Conv2D(16, (5,5), activation='relu',padding="same")(input_branch)
+    tower_2 = BatchNormalization()(tower_2)
+    tower_2 = Activation('relu')(tower_2)
+    tower_2 = AveragePooling2D(2,2)(tower_2)
+    tower_2 = Dropout(DROPOUT_VALUE)(tower_2)
+
+    tower_2 = Conv2D(32, (5,5), activation='relu',padding="same")(tower_2)
+    tower_2 = BatchNormalization()(tower_2)
+    tower_2 = Activation('relu')(tower_2)
+    tower_2 = AveragePooling2D(2,2)(tower_2)
+    tower_2 = Dropout(DROPOUT_VALUE)(tower_2)
+
+    tower_2 = Conv2D(64, (5,5), activation='relu',padding="same")(tower_2)
+    tower_2 = BatchNormalization()(tower_2)
+    tower_2 = Activation('relu')(tower_2)
+    tower_2 = AveragePooling2D(2,2)(tower_2)
+    tower_2 = Dropout(DROPOUT_VALUE)(tower_2)
+
+    tower_2 = Conv2D(64, (5,5), activation='relu',padding="same")(tower_2)
+    tower_2 = BatchNormalization()(tower_2)
+    tower_2 = Activation('relu')(tower_2)
+    tower_2 = AveragePooling2D(2,2)(tower_2)
+    tower_2 = Dropout(DROPOUT_VALUE)(tower_2)
+    # End branch 2
+
+    # Concatenate the 2 branches
+    merged = Concatenate(axis=1)([tower_1, tower_2])
+
+    # Dense layers
+    out = Flatten()(merged)
+    out = Dense(256, activation='relu')(out)
+    out = Dense(128, activation='sigmoid')(out)
+    out = Dense(64, activation='sigmoid')(out)
+    out = Dense(32, activation='sigmoid')(out)
+
+    return (input_branch, out)
+
+def get_mlp_branch(input_shape):
+    """Returns the MLP branch"""
+    input_ffnn = Input(shape=input_shape)
+
+    x = Dense(64, activation="relu")(input_ffnn)
+    x = BatchNormalization()(x)
+    x = Dropout(DROPOUT_VALUE)(x)
+
+    x = Dense(32, activation="relu")(x)
+    x = BatchNormalization()(x)
+    x = Dropout(DROPOUT_VALUE)(x)
+
+    x = Dense(16, activation="relu")(x)
+    x = BatchNormalization()(x)
+    x = Dropout(DROPOUT_VALUE)(x)
+
+    return (input_ffnn, x)
 
 # Check
 for dataset_name in ALL_DATASETS:
